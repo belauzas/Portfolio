6b39ebe77dbaf457352c39d5ff8cfa04a112cfe7
Rimantas <belauzas@yahoo.com>
2019-02-21 18:24:37 +0200

    Testimonials interactions

--- js/action.js
+++ js/action.js
@@ -14,3 +14,51 @@ setInterval(
 );
 
 $('#testimonials > .list').html( renderTestimonials( testimonials, testimonials_index ) );
+
+
+
+$('#testimonials > .tools > .fa-long-arrow-left').click(function(){
+    var index_visible = $('#testimonials > .list > .item[style="display: inline-block;"]').index(),
+        total = $('#testimonials > .list > .item').length;
+    
+    if ( index_visible > 0 ) {
+        // update bar position
+        var bar_margin = (100 / total) * (index_visible - 1);
+        $('#testimonials > .tools > .bar > .scroll').css('margin-left', bar_margin+'%');
+    
+        // update testimonials visibility
+        $('#testimonials > .list > .item').hide();
+        $('#testimonials > .list > .item').eq( index_visible - 1 ).css('display','inline-block');
+    } else {
+        // update bar position
+        var bar_margin = (100 / total) * (total - 1);
+        $('#testimonials > .tools > .bar > .scroll').css('margin-left', bar_margin+'%');
+    
+        // update testimonials visibility
+        $('#testimonials > .list > .item').hide();
+        $('#testimonials > .list > .item').eq(total - 1).css('display','inline-block');
+    }
+});
+
+$('#testimonials > .tools > .fa-long-arrow-right').click(function(){
+    var index_visible = $('#testimonials > .list > .item[style="display: inline-block;"]').index(),
+        total = $('#testimonials > .list > .item').length;
+    
+    if ( index_visible+1 < total ) {
+        // update bar position
+        var bar_margin = (100 / total) * (index_visible + 1);
+        $('#testimonials > .tools > .bar > .scroll').css('margin-left', bar_margin+'%');
+    
+        // update testimonials visibility
+        $('#testimonials > .list > .item').hide();
+        $('#testimonials > .list > .item').eq( index_visible + 1 ).css('display','inline-block');
+    } else {
+        // update bar position
+        var bar_margin = 0;
+        $('#testimonials > .tools > .bar > .scroll').css('margin-left', bar_margin+'%');
+    
+        // update testimonials visibility
+        $('#testimonials > .list > .item').hide();
+        $('#testimonials > .list > .item').eq(0).css('display','inline-block');
+    }
+});
